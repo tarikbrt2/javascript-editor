dfe9acbe5b13c74f85fd778cc401c6e86b82baa1
Tarik <tarik.brt2@gmail.com>
2021-01-15 22:26:30 +0100

    Added save button and improved style a little bit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const runButton = document.getElementById("run");
+const saveButton = document.getElementById("save");
 const consoleLogList = document.querySelector('.editor-console-body');
+const fileName = document.getElementById("editor-file-name");
 
 const defaultCode = "console.log(\"Hello world\")";
 let consoleMessages = [];
@@ -12,6 +14,15 @@ editor.setOptions({
     enableLiveAutocompletion: true
 })
 editor.setValue(defaultCode);
+
+saveButton.addEventListener("click", () => {
+    let editor_code = editor.getValue();
+    let a = document.createElement("a");
+    let file = new Blob([editor_code], {type: "text/javascript"});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName.value;
+    a.click();
+})
 
 runButton.addEventListener("click", () => {
     const code = editor.getValue();
